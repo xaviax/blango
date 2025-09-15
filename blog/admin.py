5d27acb3a062c6ff90dc268ad2d984639824efcf
Trainer Red <liquidcloud0@gmail.com>
2025-09-15 14:21:58 +0000

    Implemented Generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Tag,Post
+from .models import Tag,Post, Comment
 # Register your models here.
 
-admin.site.register(Tag)
+
 
 class PostAdmin(admin.ModelAdmin):
   prepopulated_fields={"slug":("title",)}
 
 
+admin.site.register(Tag)
+
 admin.site.register(Post,PostAdmin)
+
+admin.site.register(Comment)
