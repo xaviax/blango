4e5d0ba5820b42867f06366b03388e715dcffb7c
Trainer Red <liquidcloud0@gmail.com>
2025-09-16 14:14:01 +0000

    implemented filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,17 +1,21 @@
 from django.contrib.auth import get_user_model
 from django import template
-from django.utils.html import escape
-from django.utils.safestring import mark_safe
+#from django.utils.html import escape
+#from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 user_model=get_user_model()
 register=template.Library()
 
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user):
   
   if not isinstance(author, user_model):
 
    return ""
+
+  if author == current_user:
+    return format_html("<strong>me<strong>")
 
   if author.first_name and author.last_name:
     name=f"{author.first_name} {author.last_name}"
@@ -22,12 +26,12 @@ def author_details(author):
 
   if author.email:
 
-    email=author.email
-    prefix=f'<a href = "mailto:{email}">'
-    suffix="</a>"
+   # prefix=f'<a href = "mailto:{email}">'
+   prefix=format_html('<a href="mailto:{}">', author.email)
+   suffix=format_html("</a>")
 
   else:
     prefix=""
     suffix=""    
 
-  return mark_safe(f"{prefix}{name}{suffix}")
+  return format_html('{}{}{}',prefix,name,suffix)
