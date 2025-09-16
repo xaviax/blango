937e4fec7eb0ffd50520bc16cd459113593d3e6d
Trainer Red <liquidcloud0@gmail.com>
2025-09-16 13:07:07 +0000

    partial implemented filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+user_model=get_user_model()
+register=template.Library()
+
+
+@register.filter
+def author_details(author):
+  
+  if not isinstance(author, user_model):
+
+   return ""
+
+  if author.first_name and author.last_name:
+    name=f"{author.first_name} {author.last_name}"
+
+  else:
+    name=f"{author.username}"
+
+
+  if author.email:
+
+    email=author.email
+    prefix=f'<a href = "mailto:{email}">'
+    suffix="</a>"
+
+  else:
+    prefix=""
+    suffix=""    
+
+  return mark_safe(f"{prefix}{name}{suffix}")
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Post, Comment, Tag
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
-  return render(request,"blog/index.html")
+  posts=Post.objects.filter(published_at__lte=timezone.now())
+  return render(request,"blog/index.html",{'posts':posts})
+
