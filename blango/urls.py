e4a584f508e200f6d5bb02eb7a509a0429267deb
Trainer Red <liquidcloud0@gmail.com>
2025-09-19 13:49:14 +0000

    finished django configuration

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import blog.views
+from django.conf import settings 
+
+print(f"Time zone: {settings.TIME_ZONE}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
