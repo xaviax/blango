337633e49872422c4e31ae268ca20d8ba8daadde
Trainer Red <liquidcloud0@gmail.com>
2025-09-24 15:27:01 +0000

    implemented login and register

--- blango/settings.py
+++ blango/settings.py
@@ -197,6 +197,11 @@ class Dev(Configuration):
 
   AUTH_USER_MODEL = "blango_auth.User"
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+  ACCOUNT_ACTIVATION_DAYS = 7
+
+
  
 
 
--- blango/urls.py
+++ blango/urls.py
@@ -19,7 +19,10 @@ import blog.views
 from django.conf import settings 
 import debug_toolbar
 import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
 
+
+from blango_auth.forms import BlangoRegistrationForm
 
 print(f"Time zone: {settings.TIME_ZONE}")
 
@@ -31,6 +34,16 @@ urlpatterns = [
     path('ip/',blog.views.get_ip),
     path("accounts/",include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
+
+    path("accounts/register/",
+      
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
+
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
+
 
 ]
 
