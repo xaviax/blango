7c32996b801541b32e317fd78c17c192bba12ce0
Trainer Red <liquidcloud0@gmail.com>
2025-10-02 11:45:06 +0000

    implemented permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -2,10 +2,12 @@ from rest_framework import generics
 
 from blog.api.serializers import *
 from blog.models import *
+from blog.api.permissions import *
 
 
 class PostList(generics.ListCreateAPIView):
 
+  permission_class=[AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
   serializer_class=PostSerializer
   
