a8c42dc31b8a6bd481070891521293259f7fcfd2
Trainer Red <liquidcloud0@gmail.com>
2025-10-08 14:10:16 +0000

    finish caching

--- blog/api/views.py
+++ blog/api/views.py
@@ -7,6 +7,14 @@ from blog.api.permissions import *
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from django.utils.decorators import method_decorator 
+from django.views.decorators.cache import cache_page 
+from django.views.decorators.vary import vary_on_headers, vary_on_cookie 
+
+from rest_framework.exceptions import PermissionDenied 
+
+
+
 
 class PostViewSet(viewsets.ModelViewSet):
   permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
@@ -17,6 +25,24 @@ class PostViewSet(viewsets.ModelViewSet):
       return PostSerializer
 
     return PostDetailSerializer
+
+  @method_decorator(cache_page(300))
+  @method_decorator(vary_on_headers("Authorization"))
+  @method_decorator(vary_on_cookie)
+  @action(methods=["get"],detail=False)
+  def mine(self,request):
+    if request.user.is_anonymous:
+      raise PermissionDenied("Login to view your Posts")
+    posts= self.get_queryset().filter(author=request.user)
+    serializer=PostSerializer(posts,many=True,context={"request":request})  
+    return Response(serializer.data)
+
+  @method_decorator(cache_page(120))    
+  def list(self,*args,**kwargs):
+    return super(PostViewSet,self).list(*args,**kwargs)
+
+
+
 
 
 
@@ -39,6 +65,12 @@ class UserDetail(generics.RetrieveAPIView):
   queryset=User.objects.all()
   serializer_class=UserSerializer
 
+  @method_decorator(cache_page(300))
+  def get(self,*args,**kwargs):
+    return super(UserDetail,self).get(*args,**kwargs)
+
+
+
 
 class TagViewSet(viewsets.ModelViewSet):
 
@@ -51,4 +83,13 @@ class TagViewSet(viewsets.ModelViewSet):
     post_serializer= PostSerializer(
       tag.posts, many=True, context={"request":request}
     )
+
     return Response(post_serializer.data)
+
+  @method_decorator(cache_page(300))
+  def list(self,*args,**kwargs):
+    return super(TagViewSet,self).list(*args,**kwargs)
+
+  @method_decorator(cache_page(300))
+  def retrieve(self,*args,**kwargs):
+    return super(TagViewSet,self).retrieve(*args,**kwargs)        
