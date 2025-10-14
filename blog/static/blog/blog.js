706532c07032d263cae02cd6c0a121608077ef75
Trainer Red <liquidcloud0@gmail.com>
2025-10-14 13:36:25 +0000

    reactjs

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,2 +1,43 @@
-alert("Hello There!")
+class ClickButton extends React.Component{
+  state={
+    wasClicked:false
+  }
 
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+ render () {
+
+  let buttonText 
+  if (this.state.wasClicked){
+    buttonText="Clicked!"
+  }
+  else {
+    buttonText="Click ME!"
+  }
+
+  return React.createElement(
+    'button',
+    {
+      className: "btn btn-primary mt-2",
+      onClick: () => {
+        this.handleClick()
+      }
+    },
+    buttonText
+  )
+
+ }
+
+
+
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
